b5b7cae8ba84f114ff4f7e31d3cca98e9821e227
JP-GS <juanpgordillo89@gmail.com>
2024-07-29 11:57:16 -0300

    Renderizando las calorías consumidas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import type { Activity } from "../types"
 
 
@@ -5,7 +6,19 @@ type CalorieTrackerProps = {
     activities: Activity[]
 }
 export default function CalorieTracker({activities} : CalorieTrackerProps) {
+
+    //Contadores
+    const caloriesConsumend = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0) ,[activities])
   return (
-    <div>{activities}</div>
+    <>
+        <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+
+        <div className="flex flex-col gap-5 mt-10 items-center md:flex-row md:justify-between">
+        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+            <span className="font-black text-6xl text-orange">{caloriesConsumend}</span>
+            Consumidas
+        </p>
+        </div>
+    </>
   )
 }
